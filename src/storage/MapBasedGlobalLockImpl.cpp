9ea226eaab1196b165092ef0d377a6bdda84a1e6
igeti <pro100ia@gmail.com>
2017-10-03 23:28:38 +0300

    Add files via upload

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -1,22 +1,88 @@
 #include "MapBasedGlobalLockImpl.h"
+#include <iostream>
 
 namespace Afina {
-namespace Backend {
+    namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) { return false; }
+        bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) {
+
+            std::map<std::string, std::string>::iterator it = this->map.find(key);
+            if (it == this->map.end()) {
+                if (this->map.size() < this->_max_size) {
+                    this->map.insert(std::make_pair(key, value));
+
+                } else {
+                    this->map.erase(this -> age[0]);
+                    this->age.erase(this -> age.begin());
+                    this->map.insert(std::make_pair(key, value));
+                }
+
+            }else {
+                (it -> second) = value;
+                this->age.erase(find(age.begin(), age.end(), key));
+            }
+            this -> age.push_back(key);
+
+            return true;
+        }
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::string &value) { return false; }
+        bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::string &value) {
+
+            std::map<std::string, std::string>::iterator it = this->map.find(key);
+            if (it == this->map.end()) {
+                if (this->map.size() < this->_max_size) {
+                    this->map.insert(std::make_pair(key, value));
+                } else {
+
+                    this->map.erase(this -> age[0]);
+                    this->age.erase(this -> age.begin());
+                    this->map.insert(std::make_pair(key, value));
+                }
+            }
+            this -> age.push_back(key);
+            return true;
+        }
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) { return false; }
+        bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) {
+            std::map<std::string, std::string>::iterator it = this->map.find(key);
+            if (it != this->map.end()) {
+                it -> second = value;
+
+                this-> age.erase(find(age.begin(), age.end(), key));
+                this -> age.push_back(key);
+
+            }else {
+                return false;
+            }
+            return true;
+        }
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Delete(const std::string &key) { return false; }
+        bool MapBasedGlobalLockImpl::Delete(const std::string &key) {
+            std::map<std::string, std::string>::iterator it = this->map.find(key);
+            if (it == this->map.end()) {
+                return false;
+            }
+            this->map.erase(it);
+            this->age.erase(find(age.begin(), age.end(), key));
+
+            return true;
+        }
 
 // See MapBasedGlobalLockImpl.h
-bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const { return false; }
+        bool MapBasedGlobalLockImpl::Get(const std::string &key, std::string &value) const {
+            std::map<std::string, std::string>::const_iterator it = this->map.find(key);
+            if (it == this->map.end()) {
+                return false;
+            } else {
+                value = (it->second);
 
-} // namespace Backend
+                return true;
+            }
+        }
+
+    } // namespace Backend
 } // namespace Afina
--- src/storage/MapBasedGlobalLockImpl.h
+++ src/storage/MapBasedGlobalLockImpl.h
@@ -4,46 +4,50 @@
 #include <map>
 #include <mutex>
 #include <string>
-
+#include <queue>
 #include <afina/Storage.h>
+#include <vector>
 
 namespace Afina {
-namespace Backend {
+    namespace Backend {
 
 /**
  * # Map based implementation with global lock
  *
  *
  */
-class MapBasedGlobalLockImpl : public Afina::Storage {
-public:
-    MapBasedGlobalLockImpl(size_t max_size = 1024) : _max_size(max_size) {}
-    ~MapBasedGlobalLockImpl() {}
+        class MapBasedGlobalLockImpl : public Afina::Storage {
+        public:
+            MapBasedGlobalLockImpl(size_t max_size = 1024) : _max_size(max_size) {}
+            ~MapBasedGlobalLockImpl() {}
 
-    // Implements Afina::Storage interface
-    bool Put(const std::string &key, const std::string &value) override;
+            // Implements Afina::Storage interface
+            bool Put(const std::string &key, const std::string &value) override;
 
-    // Implements Afina::Storage interface
-    bool PutIfAbsent(const std::string &key, const std::string &value) override;
+            // Implements Afina::Storage interface
+            bool PutIfAbsent(const std::string &key, const std::string &value) override;
 
-    // Implements Afina::Storage interface
-    bool Set(const std::string &key, const std::string &value) override;
+            // Implements Afina::Storage interface
+            bool Set(const std::string &key, const std::string &value) override;
 
-    // Implements Afina::Storage interface
-    bool Delete(const std::string &key) override;
+            // Implements Afina::Storage interface
+            bool Delete(const std::string &key) override;
 
-    // Implements Afina::Storage interface
-    bool Get(const std::string &key, std::string &value) const override;
+            // Implements Afina::Storage interface
+            bool Get(const std::string &key, std::string &value) const override;
 
-private:
-    std::mutex _lock;
+        private:
+            std::mutex _lock;
 
-    size_t _max_size;
+            size_t _max_size;
 
-    std::map<std::string, std::string> _backend;
-};
+            std::vector <std::string> age;
+            std::map<std::string, std::string> map;
 
-} // namespace Backend
+
+        };
+
+    } // namespace Backend
 } // namespace Afina
 
 #endif // AFINA_STORAGE_MAP_BASED_GLOBAL_LOCK_IMPL_H
