f5e4c758bce8a09d069d5dbd830a2db110901bdc
Ivan Krylov <krylov.r00t@gmail.com>
2017-10-16 19:32:22 +0300

    improve memcached protocol conformance
    Isomorphic operations:
    
    Memcached <=> Storage
    Add        ~  PutIfAbsent
    Set        ~  Put (should *not* check for key existence)
    Replace    ~  Set
    
    Also,
    Append should check for key existence
    Get should send data in specific format

--- src/execute/Replace.cpp
+++ src/execute/Replace.cpp
@@ -6,9 +6,18 @@
 namespace Afina {
 namespace Execute {
 
+// memcached protocol:  "replace" means "store this data, but only if the server *does*
+// already hold data for this key".
+
 void Replace::Execute(Storage &storage, const std::string &args, std::string &out) {
     std::cout << "Replace(" << _key << "): " << args << std::endl;
-    out = "NOT_STORED";
+    std::string value;
+    if (storage.Get(_key, value)) {
+        storage.Set(_key, args);
+        out = "STORED";
+    } else {
+        out = "NOT_STORED";
+    }
 }
 
 } // namespace Execute
