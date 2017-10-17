966be4e3b0ee8a565d3561ecf82c6f03dd009f59
aitap <krylov.r00t@gmail.com>
2017-10-17 13:45:25 +0300

    improve memcached protocol conformance
    Isomorphic operations:
    Memcached <=> Storage
    Add        ~  PutIfAbsent
    Set        ~  Put (should *not* check for key existence)
    Replace    ~  Set
    
    Also:
    - Append should check for key existence
    - Fix Get response format

--- src/execute/Append.cpp
+++ src/execute/Append.cpp
@@ -1,16 +1,21 @@
-#include <afina/execute/Append.h>
 #include <afina/Storage.h>
+#include <afina/execute/Append.h>
 
 #include <iostream>
 
 namespace Afina {
 namespace Execute {
 
+// memcached protocol: "append" means "add this data to an existing key after existing data".
 void Append::Execute(Storage &storage, const std::string &args, std::string &out) {
     std::cout << "Append(" << _key << ")" << args << std::endl;
     std::string value;
-    storage.Get(_key, value);
-    out = storage.Put(_key, value + args);
+    if (!storage.Get(_key, value)) {
+        out.assign("NOT_STORED");
+        return;
+    }
+    storage.Put(_key, value + args);
+    out.assign("STORED");
 }
 
 } // namespace Execute
--- src/execute/Get.cpp
+++ src/execute/Get.cpp
@@ -1,25 +1,41 @@
-#include <afina/execute/Get.h>
 #include <afina/Storage.h>
+#include <afina/execute/Get.h>
 
-#include <sstream>
-#include <iterator>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 
 namespace Afina {
 namespace Execute {
 
+/* memcached protocol:
+
+Each item sent by the server looks like this:
+
+VALUE <key> <flags> <bytes>\r\n
+<data block>\r\n
+
+After all the items have been transmitted, the server sends the string
+"END\r\n"
+to indicate the end of response.
+
+*/
+
 void Get::Execute(Storage &storage, const std::string &args, std::string &out) {
     std::stringstream keyStream;
-    copy(_keys.begin(),_keys.end(), std::ostream_iterator<std::string>(keyStream," "));
+    copy(_keys.begin(), _keys.end(), std::ostream_iterator<std::string>(keyStream, " "));
     std::cout << "Get(" << keyStream.str() << ")" << std::endl;
 
     std::stringstream outStream;
 
     std::string value;
-    for (auto& key:_keys) {
-        storage.Get(key,value);
-        outStream << value << " ";
+    for (auto &key : _keys) {
+        if (!storage.Get(key, value))
+            continue;
+        outStream << "VALUE " << key << " 0 " << value.size() << "\r\n";
+        outStream << value << "\r\n";
     }
+    outStream << "END"; // networking layer should add the last \r\n
 
     out = outStream.str();
 }
