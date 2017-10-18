46dc2107604a8e3c29272d3fbece5c636c187336
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2017-10-18 18:02:06 +0000

    Updated storage test

--- test/storage/StorageTest.cpp
+++ test/storage/StorageTest.cpp
@@ -49,3 +49,78 @@ TEST(StorageTest, PutIfAbsent) {
     EXPECT_TRUE(storage.Get("KEY1", value));
     EXPECT_TRUE(value == "val1");
 }
+
+TEST(StorageTest, BigTest) {
+    MapBasedGlobalLockImpl storage(100000);
+
+    std::stringstream ss;
+
+    for(long i=0; i<100000; ++i)
+    {
+        ss << "Key" << i;
+        std::string key = ss.str();
+        ss.str("");
+        ss << "Val" << i;
+        std::string val = ss.str();
+        ss.str("");
+        storage.Put(key, val);
+    }
+    
+    for(long i=99999; i>=0; --i)
+    {
+        ss << "Key" << i;
+        std::string key = ss.str();
+        ss.str("");
+        ss << "Val" << i;
+        std::string val = ss.str();
+        ss.str("");
+        
+        std::string res;
+        storage.Get(key, res);
+
+        EXPECT_TRUE(val == res);
+    }
+
+}
+
+TEST(StorageTest, MaxTest) {
+    MapBasedGlobalLockImpl storage(1000);
+
+    std::stringstream ss;
+
+    for(long i=0; i<1100; ++i)
+    {
+        ss << "Key" << i;
+        std::string key = ss.str();
+        ss.str("");
+        ss << "Val" << i;
+        std::string val = ss.str();
+        ss.str("");
+        storage.Put(key, val);
+    }
+    
+    for(long i=100; i<1100; ++i)
+    {
+        ss << "Key" << i;
+        std::string key = ss.str();
+        ss.str("");
+        ss << "Val" << i;
+        std::string val = ss.str();
+        ss.str("");
+        
+        std::string res;
+        storage.Get(key, res);
+
+        EXPECT_TRUE(val == res);
+    }
+    
+    for(long i=0; i<100; ++i)
+    {
+        ss << "Key" << i;
+        std::string key = ss.str();
+        ss.str("");
+        
+        std::string res;
+        EXPECT_FALSE(storage.Get(key, res));
+    }
+}
