98d2ea8597dda779a32eae624be83398f929da88
aitap <krylov.r00t@gmail.com>
2017-10-19 00:25:23 +0300

    network tests: test for actually observable errors

--- test/network/test.pl
+++ test/network/test.pl
@@ -11,7 +11,7 @@ use Getopt::Long;
 
 my $backend = "blocking";
 my $silent = 0;
-my $afina = glob "$Bin/../../*/src/afina";
+my $afina = glob "$Bin/../../*/src/afina" or die "Couldn't find afina executable, please pass -a <path>\n";
 
 GetOptions(
 	"backend=s" => \$backend,
