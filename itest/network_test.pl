b2e2a31f75c8f1c7ca324cfc1f7512f9e418e637
aitap <krylov.r00t@gmail.com>
2017-11-10 23:23:48 +0300

    network tests: make it possible to test Afina with only a read-FIFO and network connection

--- itest/network_test.pl
+++ itest/network_test.pl
@@ -25,11 +25,17 @@ GetOptions(
 ) or die "Usage: $0 [-a server] [-p port]\n";
 
 sub make_afina_socket {
+	my ($write_command) = @_;
 	if (defined $rfifo and defined $wfifo) {
 		open my $wf, ">", $rfifo; # afina reads from there; we write
 		open my $rf, "<", $wfifo; # afina writes there; we read
 		return ($wf, $rf);
+	} elsif (defined $rfifo and !defined($wfifo) and $write_command) {
+		# this is a FIFO command for write-only Afina
+		open my $wf, ">", $rfifo;
+		return($wf, 1);
 	} else {
+		# testing sockets or it's a read command for write-only Afina
 		my $socket = IO::Socket::INET::->new(
 			PeerAddr => "$server:$port",
 			Proto => "tcp"
@@ -38,9 +44,9 @@ sub make_afina_socket {
 	}
 }
 
-sub afina_request { # 5 tests
-	my ($request) = @_;
-	my ($wf, $rf) = make_afina_socket;
+sub afina_request {
+	my ($request, $write_cmd) = @_;
+	my ($wf, $rf) = make_afina_socket($write_cmd);
 	ok($wf, "Connected to Afina for writing");
 	ok($rf, "Connected to Afina for reading");
 	ok(
@@ -55,7 +61,9 @@ sub afina_request { # 5 tests
 		ok(close($wf), "Closed writing end of connection");
 	}
 	my $received;
-	if ($rf->isa("IO::Socket::INET")) {
+	if ($write_cmd and defined($rfifo) and !defined($wfifo)) {
+		return;
+	} elsif ($rf->isa("IO::Socket::INET")) {
 		$received .= $_ while (<$rf>); # networked Afina must properly close socket when client is done
 	} else {
 		sysread($rf, $received, 65536); # do a single read, don't expect Afina to close fd
@@ -64,15 +72,21 @@ sub afina_request { # 5 tests
 	$received;
 }
 
-sub afina_test { # 4 tests
-	my ($request, $response, $desc) = @_;
-	ref $response ? like(afina_request($request), $response, $desc)
-		: is(afina_request($request), $response, $desc);
+sub afina_test {
+	my ($request, $response, $desc, $wr) = @_;
+	my $reply = afina_request($request, $wr);
+	if ($wr and defined($rfifo) and !defined($wfifo)) {
+		# this Afina only reads FIFO and this is a FIFO command, no reply to check
+		pass($desc)
+	} else {
+		ref $response ? like($reply, $response, $desc)
+			: is($reply, $response, $desc);
+	}
 }
 
-afina_test("set foo 0 0 6\r\nfoobar\r\n", "STORED\r\n", "Set command");
-afina_test("get foo\r\n", "VALUE foo 0 6\r\nfoobar\r\nEND\r\n", "Get the value we just set");
-afina_test("get foo\r\nget foo\r\n", "VALUE foo 0 6\r\nfoobar\r\nEND\r\nVALUE foo 0 6\r\nfoobar\r\nEND\r\n", "Multiple commands");
+afina_test("set foo 0 0 6\r\nfoobar\r\n", "STORED\r\n", "Set command", 1);
+afina_test("get foo\r\n", "VALUE foo 0 6\r\nfoobar\r\nEND\r\n", "Get the value we just set", 0);
+afina_test("get foo\r\nget foo\r\n", "VALUE foo 0 6\r\nfoobar\r\nEND\r\nVALUE foo 0 6\r\nfoobar\r\nEND\r\n", "Multiple commands", 0);
 
 afina_test(
 	"set foo 0 0 3\r\nwtf\r\n"
@@ -83,37 +97,43 @@ afina_test(
 	."VALUE foo 0 3\r\nwtf\r\n"
 	."VALUE bar 0 3\r\nzzz\r\n"
 	."END\r\n",
-	"Multiple commands with body"
+	"Multiple commands with body",
+	1
 );
 
 afina_test(
 	"add test 0 0 6\r\nfoobar\r\n",
 	"STORED\r\n",
-	"Add non-existent key"
+	"Add non-existent key",
+	1
 );
 
 afina_test(
 	"add test 0 0 6\r\nfoobar\r\n",
 	"NOT_STORED\r\n",
-	"Don't add existent key"
+	"Don't add existent key",
+	1
 );
 
 afina_test(
 	"append test_ 0 0 3\r\nwtf\r\n",
 	"NOT_STORED\r\n",
-	"Don't append non-existent key"
+	"Don't append non-existent key",
+	1
 );
 
 afina_test(
 	"append test 0 0 3\r\nwtf\r\n",
 	"STORED\r\n",
-	"Append an existent key"
+	"Append an existent key",
+	1
 );
 
 afina_test(
 	"get test\r\n",
 	"VALUE test 0 9\r\nfoobarwtf\r\nEND\r\n",
-	"Verify the append"
+	"Verify the append",
+	0
 );
 
 TODO: {
@@ -122,19 +142,22 @@ TODO: {
 	afina_test(
 		"replace test_ 0 0 3\r\nwtf\r\n",
 		"NOT_STORED\r\n",
-		"Don't replace non-existent key"
+		"Don't replace non-existent key",
+		1
 	);
 
 	afina_test(
 		"replace test 0 0 3\r\nzzz\r\n",
 		"STORED\r\n",
-		"Replace an existent key"
+		"Replace an existent key",
+		1
 	);
 
 	afina_test(
 		"get test\r\n",
 		"VALUE test 0 3\r\nzzz\r\nEND\r\n",
-		"Verify replace"
+		"Verify replace",
+		0
 	);
 }
 
@@ -144,20 +167,23 @@ TODO: {
 	afina_test(
 		"delete test\r\n",
 		"DELETED\r\n",
-		"Delete a key"
+		"Delete a key",
+		1
 	);
 }
 
 afina_test(
 	"blablabla 0 0 0\r\n",
 	qr/ERROR/,
-	"Must report unknown command error to user"
+	"Must report unknown command error to user",
+	1
 );
 
 afina_test(
 	"get var\r\r",
 	qr/ERROR/,
-	"Must report desync errors to user"
+	"Must report desync errors to user",
+	1
 );
 
 afina_test(
@@ -172,11 +198,13 @@ afina_test(
 		print $socket "\nwtf\r\n";
 	},
 	"STORED\r\n",
-	"Must correctly handle partial writes"
+	"Must correctly handle partial writes",
+	1
 );
 
 afina_test(
 	"get foo\r\n",
 	"VALUE foo 0 3\r\nwtf\r\nEND\r\n",
-	"Correct result of partially written command"
+	"Correct result of partially written command",
+	0
 );
