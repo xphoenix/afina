893dea3ccf85364e2970ea6079c34deb0c542350
romandegtyarev <roma-degtayrev@mail.ru>
2017-11-30 02:23:54 +0300

    fix

--- src/network/epoll/Worker.cpp
+++ src/network/epoll/Worker.cpp
@@ -12,6 +12,9 @@ namespace Afina {
 namespace Network {
 namespace epoll {
 
+
+
+
 // See Worker.h
 Worker::Worker(std::shared_ptr<Afina::Storage> ps) {
     // TODO: implementation here
@@ -26,18 +29,31 @@ Worker::~Worker() {
     // TODO: implementation here
 }
 
+void* Worker::RunProxy(void* _args) {
+  auto args = reinterpret_cast<std::pair<Worker*, int>*>(_args);
+  Worker* worker_instance = args->first;
+  int server_socket = args->second;
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGINT);
+  sigaddset(&mask, SIGTERM);
+  if (pthread_sigmask(SIG_BLOCK, &mask, NULL) == -1) {
+    throw std::runtime_error("pthread_sigmask");
+  }
+  worker_instance->OnRun(server_socket);
+  return NULL;
+}
 
 
 // See Worker.h
 void Worker::Start(int server_socket) {
     std::cout << "network debug: " << __PRETTY_FUNCTION__ << std::endl;
     // TODO: implementation here
-    running.store(true);
-    this->thread = pthred_create()
+    this->running.store(true);
 
     //pthread_create get pair <key, object>
     auto args = new std::pair<Worker*, int> (this, server_socket);
-    if (pthread_create( &(this->thread), NULL, &(Worker::OnRun) , args) != 0){
+    if (pthread_create( &(this->thread), NULL, &(Worker::RunProxy) , args) != 0){
         throw std::runtime_error("can't create a thread");
     }
 }
