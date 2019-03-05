7bdf21a16e31961e807f5c6285ef6ea8261bc89a
Viliars <redkillerpw@mail.ru>
2019-03-05 18:55:08 +0300

    Add new storage tests

--- test/storage/StorageTest.cpp
+++ test/storage/StorageTest.cpp
@@ -16,6 +16,9 @@ using namespace Afina::Backend;
 using namespace Afina::Execute;
 using namespace std;
 
+
+
+
 TEST(StorageTest, PutGet) {
     SimpleLRU storage;
 
@@ -89,6 +92,51 @@ TEST(StorageTest, PutDeleteGet) {
     EXPECT_FALSE(storage.Get("KEY1", value));
     EXPECT_TRUE(storage.Get("KEY2", value));
     EXPECT_TRUE(value == "val2");
+}
+
+
+TEST(StorageTest, GetIfAbsent)
+{
+    SimpleLRU storage;
+
+
+    std::string value;
+    EXPECT_FALSE(storage.Get("KEY1", value));
+
+    EXPECT_FALSE(storage.Get("KEY2", value));
+
+    EXPECT_FALSE(storage.Get("KEY3", value));
+}
+
+TEST(StorageTest, DeleteIfAbsent)
+{
+    SimpleLRU storage;
+    EXPECT_FALSE(storage.Delete("KEY1"));
+
+    EXPECT_FALSE(storage.Delete("KEY2"));
+
+    EXPECT_FALSE(storage.Delete("KEY3"));
+}
+
+TEST(StorageTest, DeleteHeadAndTailNode)
+{
+    SimpleLRU storage;
+
+    EXPECT_TRUE(storage.Put("KEY1", "val1"));
+    EXPECT_TRUE(storage.Put("KEY2", "val2"));
+    EXPECT_TRUE(storage.Put("KEY3", "val3"));
+    EXPECT_TRUE(storage.Put("KEY4", "val4"));
+
+
+    EXPECT_TRUE(storage.Set("KEY2", "val22"));
+    EXPECT_TRUE(storage.Set("KEY3", "val23"));
+    EXPECT_TRUE(storage.Set("KEY1", "val21"));
+    EXPECT_TRUE(storage.Set("KEY1", "val31"));
+    EXPECT_TRUE(storage.Set("KEY1", "val41"));
+    // After that, KEY1 should be first in the rating.
+    // And KEY4 should be the last.
+    EXPECT_TRUE(storage.Delete("KEY4"));
+    EXPECT_TRUE(storage.Delete("KEY1"));
 }
 
 std::string pad_space(const std::string &s, size_t length) {
