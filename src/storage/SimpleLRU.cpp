e15ad39cd7c0b2f125fe07267fc503108f5996b3
Rinat <Demontego>
2019-09-29 18:14:10 +0300

    HW1

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -4,19 +4,120 @@ namespace Afina {
 namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Put(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Put(const std::string &key, const std::string &value) {
+  if((key.size()+value.size())>_max_size)
+    return false;
+  if(_lru_index.find(key)!=_lru_index.end())
+    return update_node(key,value);
+  else
+    return put_node(key,value);
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) {
+  if((key.size()+value.size())>_max_size)
+    return false;
+  if(_lru_index.find(key)==_lru_index.end())
+    return put_node(key,value);
+  else
+    return false;
+ }
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Set(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Set(const std::string &key, const std::string &value) {
+  if((key.size()+value.size())>_max_size)
+    return false;
+  if(_lru_index.find(key)!=_lru_index.end())
+    return update_node(key,value);
+  else
+    return false;
+  }
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Delete(const std::string &key) { return false; }
+bool SimpleLRU::Delete(const std::string &key) {
+	auto it = _lru_index.find(key);
+    if (it == _lru_index.end()) { return false; }
+    	remove_node(it->second.get());
+    return true;
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Get(const std::string &key, std::string &value) { return false; }
+bool SimpleLRU::Get(const std::string &key, std::string &value) {
+auto it=_lru_index.find(key);
+if(it==_lru_index.end())
+  return false;
+value=it->second.get().value;
+return move_node_tail(it->second.get());
+}
 
+bool SimpleLRU::put_node(const std::string &key, const std::string &value) {
+  while(_current_size+key.size()+value.size()>_max_size)
+    remove_node(*_lru_head);
+  _current_size+=key.size()+value.size();
+  auto new_node = std::make_unique<lru_node> (key,value);
+    if( _lru_tail != nullptr)
+    {
+        new_node->prev = _lru_tail;
+        _lru_tail->next.swap(new_node);
+        _lru_tail = _lru_tail->next.get();
+
+    } else{
+        _lru_tail = new_node.get();
+        _lru_head.swap(new_node);
+    }
+
+  _lru_index.insert(std::make_pair(key,std::reference_wrapper<lru_node>(*_lru_tail)));
+  return true;
+}
+
+bool SimpleLRU::update_node(const std::string &key, const std::string &new_value){
+  lru_node& old_node=_lru_index.find(key)->second.get();
+  while(_current_size-old_node.value.size()+new_value.size()>_max_size)
+    remove_node(*_lru_head);
+  _current_size+=new_value.size()-old_node.value.size();
+  old_node.value=new_value;
+  return move_node_tail(old_node);
+}
+
+bool SimpleLRU::move_node_tail(lru_node &_node){
+  if (&_node == _lru_tail)
+    return true;
+  if(&_node==_lru_head.get()){
+    _lru_head.swap(_node.next);
+    _lru_head->prev=nullptr;
+  }
+  else{
+    _node.next->prev=_node.prev;
+    _node.prev->next.swap(_node.next);
+  }
+   _lru_tail->next.swap(_node.next);
+   _node.prev=_lru_tail;
+   _lru_tail=&_node;
+   return true;
+}
+
+bool SimpleLRU::remove_node(lru_node &delete_node){
+  _lru_index.erase(delete_node.key);
+  _current_size-=((_lru_tail->key).size() + (_lru_tail->value).size());
+  std::unique_ptr<lru_node> tmp;
+  if(&delete_node==_lru_head.get())
+  {
+    tmp.swap(_lru_head);
+    _lru_head.swap(tmp->next);
+    _lru_head->prev=nullptr;
+    return true;
+  }
+  else if(&delete_node==_lru_tail )
+  {
+    tmp.swap(_lru_tail->prev->next);
+    _lru_tail=tmp->prev;
+    _lru_tail->next.reset(nullptr);
+    return true;
+  }
+  tmp.swap(delete_node.prev->next);
+  tmp->prev->next.swap(tmp->next);
+  tmp->next->prev=tmp->prev;
+  return true;
+}
 } // namespace Backend
 } // namespace Afina
--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -5,6 +5,8 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <utility>
+#include <iostream>
 
 #include <afina/Storage.h>
 
@@ -17,11 +19,23 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    SimpleLRU(size_t max_size = 1024) :
+	_max_size(max_size),_current_size(0), _lru_head(nullptr),_lru_tail(nullptr) {}
 
     ~SimpleLRU() {
-        _lru_index.clear();
-        _lru_head.reset(); // TODO: Here is stack overflow
+		if (_lru_head == nullptr) {
+		return;
+	}
+	_lru_index.clear();
+
+
+	while(_lru_head->next != nullptr )
+	{
+		std::unique_ptr<lru_node> node_to_delete;
+		node_to_delete.swap(_lru_head);
+		_lru_head.swap(node_to_delete->next);
+	}
+	_lru_head.reset();
     }
 
     // Implements Afina::Storage interface
@@ -44,24 +58,30 @@ private:
     using lru_node = struct lru_node {
         std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node *prev;
         std::unique_ptr<lru_node> next;
+		lru_node (const std::string &k,const std::string &v)
+        :key(k), value(v), prev(nullptr),next(nullptr){}
     };
 
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
-    std::size_t _max_size;
+    std::size_t _max_size, _current_size;
 
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
     std::unique_ptr<lru_node> _lru_head;
-
+    lru_node* _lru_tail;
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
-};
+    std::map<std::string,std::reference_wrapper<lru_node> > _lru_index;
 
+    bool put_node(const std::string &key, const std::string &value);
+    bool update_node(const std::string &key,const std::string &value);
+    bool remove_node(lru_node &delete_node);
+    bool move_node_tail(lru_node &node);
+};
 } // namespace Backend
 } // namespace Afina
 
