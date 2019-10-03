7d34f7120d882a303a332643f0f2ab389f56429a
Evgeniy Kolmagorov <appleforipad@inbox.ru>
2019-10-03 12:37:26 +0300

    add private functions for list

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -5,8 +5,9 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <iostream>
 
-#include <afina/Storage.h>
+#include "afina/Storage.h"
 
 namespace Afina {
 namespace Backend {
@@ -15,16 +16,17 @@ namespace Backend {
  * # Map based implementation
  * That is NOT thread safe implementaiton!!
  */
+
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    SimpleLRU(size_t max_size = 1024) :
+     _max_size(max_size), _busy_memory(0) {}
 
-    ~SimpleLRU() {
-        _lru_index.clear();
-        _lru_head.reset(); // TODO: Here is stack overflow
-    }
+
+    ~SimpleLRU();
 
     // Implements Afina::Storage interface
+    //add const
     bool Put(const std::string &key, const std::string &value) override;
 
     // Implements Afina::Storage interface
@@ -39,27 +41,42 @@ public:
     // Implements Afina::Storage interface
     bool Get(const std::string &key, std::string &value) override;
 
+    //Print all items in Storage
+    void PrintStorage();
+
 private:
     // LRU cache node
     using lru_node = struct lru_node {
         std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node* prev;
         std::unique_ptr<lru_node> next;
     };
 
+
+
+    using string_wrapper = std::reference_wrapper<const std::string>;
+    using node_wrapper = std::reference_wrapper<lru_node>;
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
     std::size_t _max_size;
+    //Save number of busy bytes
+    std::size_t _busy_memory;
 
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
     std::unique_ptr<lru_node> _lru_head;
-
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+    std::map< string_wrapper, node_wrapper, std::less<std::string>> _lru_index;
+
+    //Function that free last elements;
+    //Returns number realised bytes
+    size_t _freeTail(const size_t req_mem);
+    //Add node at head of list
+    void _addNode(const std::string &key, const std::string &value);
+
 };
 
 } // namespace Backend
