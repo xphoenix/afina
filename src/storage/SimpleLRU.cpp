dd3b8aa5486cd941f36c661ba2e77fd678f9f680
alexrez <reznikova.am@gmail.com>
2019-10-05 17:16:08 +0300

    {}  was added

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -8,11 +8,15 @@ namespace Backend {
 void SimpleLRU::NodeTransfer(lru_node &node)
 {
     if(node.prev == nullptr)
+    {
         return; // first node in list
+    }
 
     _lru_head->prev = &node;
     if(node.next)
+    {
         node.next->prev = node.prev;
+    }
 
     std::unique_ptr<lru_node> tmp_ptr = std::move(node.prev->next);
     node.prev->next = std::move(node.next);
@@ -25,7 +29,9 @@ SimpleLRU::lru_node *SimpleLRU::MakeNode(const std::string &key, const std::stri
 {
     lru_node *new_node = new lru_node(key);
     if(_lru_head)
+    {
         _lru_head->prev = new_node;
+    }
     // new_node->key = key;
     new_node->value = value;
     new_node->prev = nullptr;
@@ -40,7 +46,9 @@ void SimpleLRU::DropNodes(std::size_t size)
   while (_lru_head && (_cur_size + size > _max_size))
   {
       while(last_node->next.get() != nullptr)
+      {
           last_node = last_node->next.get();
+      }
 
       _cur_size -= last_node->key.size();
       _cur_size -= last_node->value.size();
@@ -48,9 +56,13 @@ void SimpleLRU::DropNodes(std::size_t size)
 
       last_node = last_node->prev;
       if (last_node == nullptr)
+      {
           _lru_head.reset(nullptr);
+      }
       else
+      {
           last_node->next.reset(nullptr);
+      }
   }
 }
 
@@ -59,7 +71,9 @@ void SimpleLRU::DropNodes(std::size_t size)
 bool SimpleLRU::Put(const std::string &key, const std::string &value)
 {
     if (key.size() + value.size() > _max_size)
+    {
         throw std::runtime_error("Limit size exceeded");
+    }
 
     auto map_it = _lru_index.find(key);
     std::size_t put_size = value.size();
@@ -71,14 +85,18 @@ bool SimpleLRU::Put(const std::string &key, const std::string &value)
         out_size = map_it->second.get().value.size();
     }
     else
+    {
         put_size += key.size();
+    }
     // if necessary will be free size in cache
     DropNodes(put_size - out_size);
 
     _cur_size += put_size;
     _cur_size -= out_size;
     if (map_it != _lru_index.end())
+    {
         map_it->second.get().value = value;
+    }
     else
     {
         lru_node *node = MakeNode(key, value);
@@ -92,10 +110,14 @@ bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value)
 {
     std::size_t put_size = value.size() + key.size();
     if (put_size > _max_size)
+    {
         throw std::runtime_error("Limit size exceeded");
-        // return false;
+      // return false;
+    }
     if(_lru_index.find(key) != _lru_index.end())
+    {
         return false;
+    }
     // if necessary will be free size in cache
     DropNodes(put_size);
 
@@ -112,10 +134,14 @@ bool SimpleLRU::Set(const std::string &key, const std::string &value)
     std::size_t out_size = 0;
     auto map_it = _lru_index.find(key);
     if (put_size > _max_size)
+    {
         throw std::runtime_error("Limit size exceeded");
         // return false;
+    }
     if (map_it == _lru_index.end())
+    {
         return false;
+    }
 
     NodeTransfer(map_it->second.get());
     out_size = map_it->second.get().value.size();
@@ -133,7 +159,9 @@ bool SimpleLRU::Delete(const std::string &key)
 {
     auto map_it = _lru_index.find(key);
     if (map_it == _lru_index.end())
+    {
         return false;
+    }
 
     lru_node &node_to_drop = map_it->second.get();
     _cur_size -= key.size() + node_to_drop.value.size();
@@ -141,12 +169,18 @@ bool SimpleLRU::Delete(const std::string &key)
     _lru_index.erase(map_it);
 
     if(node_to_drop.next)
+    {
         node_to_drop.next->prev = node_to_drop.prev;
+    }
 
     if(node_to_drop.prev == nullptr)
+    {
         _lru_head = std::move(node_to_drop.next);
+    }
     else
+    {
         node_to_drop.prev->next = std::move(node_to_drop.next);
+    }
 
     return true;
 }
@@ -156,8 +190,9 @@ bool SimpleLRU::Get(const std::string &key, std::string &value)
 {
     auto map_it = _lru_index.find(key);
     if (map_it == _lru_index.end())
+    {
         return false;
-
+    }
     value = map_it->second.get().value;
     return true;
 }
