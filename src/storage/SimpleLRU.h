e09507151fc4bc19f56e898606c9aa763b42e379
r.allayarov <rusallasad@gmail.com>
2019-10-12 14:28:25 +0300

    Fixed deleting

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -1,6 +1,7 @@
 #ifndef AFINA_STORAGE_SIMPLE_LRU_H
 #define AFINA_STORAGE_SIMPLE_LRU_H
 
+#include <iostream>
 #include <map>
 #include <memory>
 #include <mutex>
@@ -20,17 +21,18 @@ public:
     SimpleLRU(size_t max_size = 1024)
 		: _max_size(max_size), _free_size(max_size), _lru_head(nullptr), _lru_tail(nullptr) {}
 
-    ~SimpleLRU() {
-        _lru_index.clear();
-        while (_lru_head != nullptr && _lru_head->next != nullptr){
-			std::unique_ptr<lru_node> tmp = nullptr;
-			_lru_head->next.swap(tmp);
-			_lru_head.swap(tmp);
-			tmp.reset();	
-		}
-		_lru_head.reset(); // TODO: Here is stack overflow
-    }
-
+	~SimpleLRU() {
+		if (_lru_head) {
+        	_lru_index.clear(); // TODO: Here is stack overflow	            _lru_index.clear();
+            while (_lru_head->next != nullptr) {
+            	std::unique_ptr<lru_node> node_to_delete;
+            	node_to_delete.swap(_lru_head);
+            	_lru_head.swap(node_to_delete->next);
+            }
+            _lru_head.reset();
+        }
+    };
+public:
     // Implements Afina::Storage interface
     bool Put(const std::string &key, const std::string &value) override;
 
@@ -49,7 +51,7 @@ public:
 private:
     // LRU cache node
     using lru_node = struct lru_node {
-        const std::string key;
+        std::string key;
         std::string value;
         lru_node *prev;
 		//std::unique_ptr<lru_node> prev;
@@ -59,16 +61,16 @@ private:
 			:key(key), value(value), prev(nullptr), next(nullptr) {}
 
     };
-
+private:
 	bool _insert_kv(const std::string &key, const std::string &value);
-	bool _delete_oldest();
 	bool _insert(lru_node &node);
-	bool _delete(lru_node &node);
 	bool _update_kv(const std::string &key, const std::string &value);
 	bool _move_to_tail(lru_node &node);	
+	bool remove_node(lru_node &delete_node);
 	// Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
-    std::size_t _max_size;
+private:
+	std::size_t _max_size;
 
 	std::size_t _free_size;
 
@@ -76,12 +78,13 @@ private:
     // element that wasn't used for longest time.
     //
     // List owns all nodes
-    std::unique_ptr<lru_node> _lru_head;
-    lru_node *_lru_tail;
+	std::unique_ptr<lru_node> _lru_head;
+	lru_node *_lru_tail;
 
 
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+	using mapT = std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>;
+	mapT _lru_index;
 };
 
 } // namespace Backend
