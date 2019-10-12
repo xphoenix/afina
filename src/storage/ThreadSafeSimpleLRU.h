f8b47267e229f8ab2a0932a2208ce0d5ed2c9d8e
alexrez <reznikova.am@gmail.com>
2019-10-12 14:06:57 +0300

    modified lock/unlock to functions

--- src/storage/ThreadSafeSimpleLRU.h
+++ src/storage/ThreadSafeSimpleLRU.h
@@ -21,119 +21,106 @@ class ThreadSafeSimplLRU : public SimpleLRU {
 public:
     ThreadSafeSimplLRU(size_t max_size = 1024) :
         SimpleLRU(max_size),
-        _read_flag(false),
         _readers(0),
         _writers(0)
         {}
     ~ThreadSafeSimplLRU() {}
 
+    void lock(){
+        std::unique_lock<std::mutex> locker(_mutex);
+        while(_writers != 0)
+        {
+            _cv_read.wait(locker);
+        }
+        _writers++;
+        while(_readers != 0)
+        {
+            _cv_write.wait(locker);
+        }
+    }
+
+    void unlock(){
+        {
+            std::unique_lock<std::mutex> locker(_mutex);
+              _writers--;
+        }
+            _cv_read.notify_all();
+    }
+
+    void shared_lock(){
+        std::unique_lock<std::mutex> locker(_mutex);
+        while (_writers != 0)
+        {
+            _cv_read.wait(locker);
+        }
+        _readers++;
+    }
+
+    void shared_unlock(){
+        std::unique_lock<std::mutex> locker(_mutex);
+        _readers--;
+        if (_writers != 0)
+        {
+            if (_readers == 0)
+            {
+                _cv_write.notify_one();
+            }
+        }
+    }
+
     // see SimpleLRU.h
     bool Put(const std::string &key, const std::string &value) override {
+        lock();
         bool result;
         {
             std::unique_lock<std::mutex> locker(_mutex);
-            while(_writers != 0)
-            {
-                _cv_read.wait(locker);
-            }
-            _writers++;
-            while(_readers != 0)
-            {
-                _cv_write.wait(locker);
-            }
             result = SimpleLRU::Put(key, value);
-            _writers--;
         }
-        _cv_read.notify_all();
+        unlock();
         return result;
     }
 
     // see SimpleLRU.h
     bool PutIfAbsent(const std::string &key, const std::string &value) override {
+        lock();
         bool result;
         {
             std::unique_lock<std::mutex> locker(_mutex);
-            while(_writers != 0)
-            {
-                _cv_read.wait(locker);
-            }
-            _writers++;
-            while(_readers != 0)
-            {
-                _cv_write.wait(locker);
-            }
             result = SimpleLRU::PutIfAbsent(key, value);
-            _writers--;
         }
-        _cv_read.notify_all();
+        unlock();
         return result;
     }
 
     // see SimpleLRU.h
     bool Set(const std::string &key, const std::string &value) override {
+        lock();
         bool result;
         {
             std::unique_lock<std::mutex> locker(_mutex);
-            while(_writers != 0)
-            {
-                _cv_read.wait(locker);
-            }
-            _writers++;
-            while(_readers != 0)
-            {
-                _cv_write.wait(locker);
-            }
             result = SimpleLRU::Set(key, value);
-            _writers--;
         }
-        _cv_read.notify_all();
+        unlock();
         return result;
     }
 
     // see SimpleLRU.h
     bool Delete(const std::string &key) override {
+        lock();
         bool result;
         {
             std::unique_lock<std::mutex> locker(_mutex);
-            while(_writers != 0)
-            {
-                _cv_read.wait(locker);
-            }
-            _writers++;
-            while(_readers != 0)
-            {
-                _cv_write.wait(locker);
-            }
             result = SimpleLRU::Delete(key);
-            _writers--;
         }
-        _cv_read.notify_all();
+        unlock();
         return result;
     }
 
     // see SimpleLRU.h
     bool Get(const std::string &key, std::string &value) override {
-        {
-            std::unique_lock<std::mutex> locker(_mutex);
-            while (_writers != 0)
-            {
-                _cv_read.wait(locker);
-            }
-            _readers++;
-
-        }
-        auto result = SimpleLRU::Get(key, value);
-        {
-            std::unique_lock<std::mutex> locker(_mutex);
-            _readers--;
-            if (_writers != 0)
-            {
-                if (_readers == 0)
-                {
-                    _cv_write.notify_one();
-                }
-            }
-        }
+        shared_lock();
+        bool result = SimpleLRU::Get(key, value);
+        shared_unlock();
         return result;
     }
 
@@ -141,10 +128,10 @@ private:
     // Mutex for storage operations
     std::mutex _mutex;
 
-    std::atomic<bool> _read_flag;
-
+    // Number of threads that wait to get data
     uint32_t _readers;
 
+    // Number of threads that wait to put/set/delete data
     uint32_t _writers;
 
     //
