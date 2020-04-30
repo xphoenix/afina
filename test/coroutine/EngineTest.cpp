5d51ff38df2018cfbe15a37860bd2dc5301733ea
NastyBoget <bogatenkova.anastasiya@mail.ru>
2020-04-30 20:28:01 +0300

    block/unblock coroutine test added

--- test/coroutine/EngineTest.cpp
+++ test/coroutine/EngineTest.cpp
@@ -61,3 +61,45 @@ TEST(CoroutineTest, Printer) {
     engine.start(_printer, engine, result);
     ASSERT_STREQ("A1 B1 A2 B2 A3 B3 END", result.c_str());
 }
+
+std::stringstream out1;
+void *pc = nullptr, *pd = nullptr;
+
+void printc(Afina::Coroutine::Engine &pe, std::stringstream &out, void *&other) {
+    out << "C1 ";
+    pe.block(nullptr);
+    out << "C2 ";
+    pe.unblock(other);
+}
+
+void printd(Afina::Coroutine::Engine &pe, std::stringstream &out, void *&other) {
+    out << "D1 ";
+    pe.unblock(other);
+    pe.block(nullptr);
+    out << "D2 ";
+}
+
+void block_printer(Afina::Coroutine::Engine &pe, std::string &result) {
+    pc = pe.run(printc, pe, out1, pd);
+    pd = pe.run(printd, pe, out1, pc);
+
+    // Pass control to first routine, it will ping pong
+    // between printa/printb greedely then we will get
+    // control back
+    pe.sched(pc);
+
+    out1 << "END";
+
+    // done
+    result = out1.str();
+
+}
+
+TEST(CoroutineTest, Block) {
+    Afina::Coroutine::Engine engine;
+
+    std::string result;
+    engine.start(block_printer, engine, result);
+
+    ASSERT_STREQ("C1 D1 C2 D2 END", result.c_str());
+}
