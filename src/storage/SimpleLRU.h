1835d4f0f85a79cced42b80110961cb92558c194
artei-solod <a.solodukhov@gmail.com>
2020-09-23 13:32:35 +0300

    storage

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -1,10 +1,12 @@
 #ifndef AFINA_STORAGE_SIMPLE_LRU_H
 #define AFINA_STORAGE_SIMPLE_LRU_H
 
+#include <iostream>
 #include <map>
 #include <memory>
 #include <mutex>
 #include <string>
+#include <utility>
 
 #include <afina/Storage.h>
 
@@ -17,11 +19,19 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    
+    SimpleLRU(size_t max_size = 1024) : _max_size(max_size), _current_size(0), _lru_head(nullptr), _lru_tail(nullptr) {}
 
-    ~SimpleLRU() {
-        _lru_index.clear();
-        _lru_head.reset(); // TODO: Here is stack overflow
+     ~SimpleLRU() {
+        if (_lru_head) {
+            _lru_index.clear();
+            while (_lru_head->next != nullptr) {
+                std::unique_ptr<lru_node> node_to_delete;
+                node_to_delete.swap(_lru_head);
+                _lru_head.swap(node_to_delete->next);
+            }
+            _lru_head.reset();
+        }
     }
 
     // Implements Afina::Storage interface
@@ -41,27 +51,32 @@ public:
 
 private:
     // LRU cache node
-    using lru_node = struct lru_node {
+    u using lru_node = struct lru_node {
         std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node *prev;
         std::unique_ptr<lru_node> next;
+        lru_node(const std::string &k, const std::string &v) : key(k), value(v), prev(nullptr), next(nullptr) {}
     };
-
+    using mapT = std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>,
+                          std::less<const std::string>>;
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
-    std::size_t _max_size;
+    std::size_t _max_size, _current_size;
 
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
-    std::unique_ptr<lru_node> _lru_head;
-
+    lru_node *_lru_tail;
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
-};
+    mapT _lru_index;
 
+    bool put_node(const std::string &key, const std::string &value);
+    bool update_node(const mapT::iterator &it, const std::string &value);
+    bool remove_node(lru_node &delete_node);
+    bool move_node_tail(lru_node &node);
+};
 } // namespace Backend
 } // namespace Afina
 
