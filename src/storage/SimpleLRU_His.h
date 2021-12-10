dd81cddea3c536ddabc7a0a4f570c40adddca3bf
krytoy4yvak <user02076@gmail.com>
2021-12-10 18:22:45 +0300

    l1

---
+++ src/storage/SimpleLRU_His.h
@@ -0,0 +1,68 @@
+#ifndef AFINA_STORAGE_SIMPLE_LRU_H
+#define AFINA_STORAGE_SIMPLE_LRU_H
+
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+
+#include <afina/Storage.h>
+
+namespace Afina {
+namespace Backend {
+
+/**
+ * # Map based implementation
+ * That is NOT thread safe implementaiton!!
+ */
+class SimpleLRU : public Afina::Storage {
+public:
+    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+
+    ~SimpleLRU() {
+        _lru_index.clear();
+        _lru_head.reset(); // TODO: Here is stack overflow
+    }
+
+    // Implements Afina::Storage interface
+    bool Put(const std::string &key, const std::string &value) override;
+
+    // Implements Afina::Storage interface
+    bool PutIfAbsent(const std::string &key, const std::string &value) override;
+
+    // Implements Afina::Storage interface
+    bool Set(const std::string &key, const std::string &value) override;
+
+    // Implements Afina::Storage interface
+    bool Delete(const std::string &key) override;
+
+    // Implements Afina::Storage interface
+    bool Get(const std::string &key, std::string &value) override;
+
+private:
+    // LRU cache node
+    using lru_node = struct lru_node {
+        std::string key;
+        std::string value;
+        std::unique_ptr<lru_node> prev;
+        std::unique_ptr<lru_node> next;
+    };
+
+    // Maximum number of bytes could be stored in this cache.
+    // i.e all (keys+values) must be not greater than the _max_size
+    std::size_t _max_size;
+
+    // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
+    // element that wasn't used for longest time.
+    //
+    // List owns all nodes
+    std::unique_ptr<lru_node> _lru_head;
+
+    // Index of nodes from list above, allows fast random access to elements by lru_node#key
+    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+};
+
+} // namespace Backend
+} // namespace Afina
+
+#endif // AFINA_STORAGE_SIMPLE_LRU_H
